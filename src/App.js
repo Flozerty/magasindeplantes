cd714c880fd05123cef7f08f73c3b31df87f8199
Floris Louërat <floris.louerat@gmail.com>
2023-05-02 15:44:12 +0200

    blabla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+function Header() {
+  return (
+    <div><h1>La maison Jungle</h1></div>
+  )
+}
+
+function Description() {
+  return (<p>Ici achetez toutes les plantes dont vous avez toujours rêvé!</p>)
+}
+
+function Banner() {
+  return (<div>
+    <Header/>
+    <Description/>
+  </div>)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <div>{ 2 > 0 ? 'Deux est plus grand que zéro' : "Ceci n'apparaîtra jamais" }</div>
     </div>
   );
 }
